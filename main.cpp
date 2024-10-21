dd17e622d1cf7c33f55b4a7eef449e70b1a99a8d
KovDarlin <ki27.kovalenko.daryna@vtc.vn.ua>
2024-10-21 22:12:28 +0300

    ex 1 was done

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
+	string first = "Lorem ipsum odor amet, consectetuer adipiscing elit.";
+	cout << "Main string: " << first<< endl; 
+	for (char& bck : first) {
+		if (bck == ' ') {
+			bck = '\t';
+		}
 
+	}
+	cout << "New string: " << first << endl;
 }
