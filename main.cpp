ad5bfda07c41b4ee9ccdabc3adfe860b9cfa4c6a
KovDarlin <ki27.kovalenko.daryna@vtc.vn.ua>
2024-10-21 23:51:44 +0300

    ex2 was done

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main() {
 
+void delete_sym(string& str, int pos) {
+	if (pos > str.length() || pos < 0) {
+		cout << "O no Problem!" << endl;
+		return;
+	}
+	str.erase(pos, 1);
+}
+
+int main() {
+	string first;
+	int pos;
+
+	cout << "Enter string: ";
+	getline(cin, first);
+
+	cout << "Enter position for symbol delete: ";
+	cin >> pos;
+
+	delete_sym(first, pos);
+	cout << "Your new string: " << first << endl;
 }
