65df7b82aca3db6b93c6daf095e2dae2a3231ea6
KovDarlin <ki27.kovalenko.daryna@vtc.vn.ua>
2024-10-21 23:06:45 +0300

    ex 4 was done

--- main.cpp
+++ main.cpp
@@ -1,6 +1,45 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main() {
+bool palidrom(const string& str) {
+	int l = 0;
+	int r = str.length() - 1;
+	while (l < r) {
+		if (str[l] == ' ') {
+			l++;
+			continue;
+		}
+		if (str[r] == ' ') {
+			r--;
+			continue;
+		}
+		if (tolower(str[l]) != tolower(str[r])) {
+			return false;
+		}
+		l++;
+		r--;
+	}
+	return true;
+}
 
+int main() {
+	string first = "Anna";
+	cout << first << endl;
+	if (palidrom(first)) {
+		cout << "Palindrom" << endl;
+	}
+	else {
+		cout << "NO Palindrom" << endl;
+	}
+
+
+	string second = "Hasan";
+	cout << second << endl;
+	if (palidrom(second)) {
+		cout << "Palindrom" << endl;
+	}
+	else {
+		cout << "NO Palindrom" << endl;
+	}
 }
