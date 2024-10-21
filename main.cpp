2f9cbf925b6d9cef35a60c7aed81f8ba3c382009
KovDarlin <ki27.kovalenko.daryna@vtc.vn.ua>
2024-10-21 23:31:34 +0300

    ex 4 was done

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
+	string first;
+	cout << "Enter string: ";
+	getline(cin, first);
+
+	for (char& bck : first) {
+		if (bck == '.') {
+			bck = '!';
+		}
+
+	}
+	cout << "Count symbols: " << first << endl;
 
 }
