df06e8c7c80f500a9c5aec639f358c214ec15ac2
KovDarlin <ki27.kovalenko.daryna@vtc.vn.ua>
2024-10-21 23:26:35 +0300

    ex 5 was done

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
+	int count = 0;
+	string first;
+	char symbol;
 
+	cout << "Enter string: ";
+	getline(cin, first);
+
+	cout << "Enter symbol: ";
+	cin >> symbol;
+
+	for (char& bck : first) {
+		if (bck == symbol) {
+			count++;
+		}
+
+	}
+	cout << "Count symbols: " << count << endl;
+	
 }
