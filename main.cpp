5a2891bf0f519948b4a550ff3b1355e7b9dcbb09
KovDarlin <ki27.kovalenko.daryna@vtc.vn.ua>
2024-10-22 00:02:15 +0300

    ex 1 was done

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main() {
 
+void del(string& str, char sym) {
+	str.erase(remove(str.begin(), str.end(), sym), str.end());
+}
+
+int main() {
+	string first;
+	char symbol;
+
+	cout << "Enter string: ";
+	getline(cin, first);
+
+	cout << "Enter symbol for delete: ";
+	cin >> symbol;
+
+	del(first, symbol);
+	cout << "Your new string: " << first << endl;
 }
