67699b1934969ea15a896413d9ffcc9c2807be0f
KovDarlin <ki27.kovalenko.daryna@vtc.vn.ua>
2024-10-21 22:44:59 +0300

    ex 3 was done

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
+	int count = 0;
+	bool f = true;
+	string first = "Lorem ipsum odor amet, consectetuer adipiscing elit.";
+	cout << first << endl;
+	for (int i = 0; i < first.length(); i++) {
+		if ((first[i] == ' ') || (first[i] == ',') || (first[i] == '.') || (first[i] == '!') || (first[i] == '?')) {
+			f = true;
+		}
+		else {
+			if (f) {
+				count++;
+				f = false;
+			}
+		}
+	}
+	cout << count << endl;
 
 }
