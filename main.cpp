d0707371f8eed92211bf2fb828d23ee630ad2bed
KovDarlin <ki27.kovalenko.daryna@vtc.vn.ua>
2024-10-21 23:16:20 +0300

    ex 6 was done

--- main.cpp
+++ main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <string>
+#include <cctype>
 using namespace std;
 
 int main() {
+	int litera = 0;
+	int cifra = 0;
+	int inshe = 0;
+	string first;
+	cout << "Enter string: " ;
+	getline(cin, first);
+	for (char& bck : first) {
+		if (bck == ' ') {
+			continue;
+		}
+		if (isalpha(bck)) {
+			litera++;
+		}
+		else if (isdigit(bck)) {
+			cifra++;
+		}
+		else {
+			inshe++;
+		}
 
+	}
+	cout << "Count letters: " << litera << endl;
+	cout << "Count numbers: " << cifra << endl;
+	cout << "Count anoters : " << inshe << endl;
 }
