0dd6f8b141a3f0b9e04b11c940b2b61139881d5f
KovDarlin <ki27.kovalenko.daryna@vtc.vn.ua>
2024-10-21 23:47:00 +0300

    ex 3 was done

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main() {
 
+void symbol_pos(string& str, char sym, int pos) {
+	if (pos > str.length() || pos < 0) {
+		cout << "O no Problem!" << endl;
+		return;
+	}
+	str.insert(pos, 1, sym);
+}
+
+int main() {
+	string first;
+	char symbol;
+	int pos;
+
+	cout << "Enter string: ";
+	getline(cin, first);
+
+	cout << "Enter symbol: ";
+	cin >> symbol;
+
+	cout << "Enter position for symbol: ";
+	cin >> pos;
+
+	symbol_pos(first, symbol, pos);
+	cout << "Your new string: "<< first<<endl;
 }
